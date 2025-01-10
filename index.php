fca12565ac87275ebbcf7ba1009d870e7c3d3062
Bremzen <aleksisdz6@gmail.com>
2025-01-10 20:29:42 +0200

    //added sticky navbar and image overlay fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Projekta mājaslapa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css?v=2">
   </head>
   <body>
 
@@ -42,6 +42,16 @@
 
     <div class="hero-section">
       <div class="image"></div>
+      <div class="container px-4 py-3">
+        <div class="row gx-5">
+          <div class="col">
+           <div class="p-3 border bg-light">test padding</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Custom column padding</div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <!-- End of Hero Section-->
