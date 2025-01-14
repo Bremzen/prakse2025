a64babd61bb9e8c2e00db39f44e0d5bd8851235f
Bremzen <aleksisdz6@gmail.com>
2025-01-14 16:00:31 +0200

    //added new sections and navbar fix

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
     <!-- Navigation bar -->
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">SIA Uzņēmums</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,16 +21,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Sākums</a>
+              <a class="nav-link active" aria-current="page" href="#sakums">Sākums</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Produkti</a>
+              <a class="nav-link active" aria-current="page" href="#produkti">Produkti</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Galerija</a>
+              <a class="nav-link active" aria-current="page" href="#galerija">Galerija</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Kontakti</a>
+              <a class="nav-link active" aria-current="page" href="#kontakti">Kontakti</a>
             </li>
           </ul>
         </div>
@@ -41,7 +41,7 @@
 
     <!-- Hero Section -->
 
-    <div class="hero-section">
+    <div class="hero-section" id="sakums">
       <div class="image"></div>
       <div class="container px-4 py-3">
         <div class="row gx-5">
@@ -59,7 +59,7 @@
 
     <!-- Product Section -->
 
-    <h1>Produkti</h1>
+    <h1 class="display-1" id="produkti">Produkti</h1>
     <div class="container-fluid row">
       <div class="card col" style="width: 18rem;">
         <img src="https:/picsum.photos/200" class="card-img-top" alt="Milk">
@@ -95,24 +95,43 @@
 
     <!-- Carousel -->
 
-    <h1>Karuselis</h1>
+    <h1 id="galerija">Galerija</h1>
     
       <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="...">
+            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="Photo 1">
           </div>
           <div class="carousel-item">
-            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="...">
+            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="Photo 2">
           </div>
           <div class="carousel-item">
-            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="...">
+            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="Photo 3">
           </div>
         </div>
       </div>
     
 
     <!-- End of Carousel -->
+
+    <!-- Contact form -->
+
+    
+    <!--Forma ar laukiem: Vārds, E-pasts, Ziņojums un Poga "Sūtīt". -->
+    <h1 class="display-2">Sazinies ar mums</h1>
+    <div class="mb-3">
+      <label for="vardsInput" class="form-label">Vārds</label>
+      <input type="email" class="form-control" id="vardsInput" placeholder="Jānis">
+    </div>
+    <div class="mb-3">
+      <label for="epastsInput" class="form-label">E-pasts</label>
+      <input type="email" class="form-control" id="epastsInput" placeholder="vards@piemers.com">
+    </div>
+    <div class="mb-3">
+      <label for="zinojums" class="form-label">Ziņojums</label>
+      <textarea class="form-control" id="zinojums" rows="2"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Sūtīt</button>
 
   </body>
 </html>
