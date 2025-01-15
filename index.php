a455c4318be69a4f26918c54adb67bc2c6100e9d
Bremzen <aleksisdz6@gmail.com>
2025-01-15 13:35:42 +0200

    //improved general look and added content

--- index.php
+++ index.php
@@ -44,13 +44,10 @@
     <div class="hero-section" id="sakums">
       <div class="image"></div>
       <div class="container px-4 py-3">
-        <div class="row gx-5">
-          <div class="col">
-           <div class="p-3 border bg-light">test padding</div>
-          </div>
-          <div class="col">
-            <div class="p-3 border bg-light">Custom column padding</div>
-          </div>
+        <div class="text-center">
+          <h1 class="display-1">Pērc mūsu produktus :thumbsup:</h1>
+          <h3 class="py-5">Mums ir labi produkti, lūdzu pērc tos.</h3>
+          <a class="btn btn-primary" href="#produkti" role="button">Skatīt produktus</a>
         </div>
       </div>
     </div>
@@ -60,78 +57,105 @@
     <!-- Product Section -->
 
     <h1 class="display-1" id="produkti">Produkti</h1>
-    <div class="container-fluid row">
-      <div class="card col" style="width: 18rem;">
+
+    <div class="d-flex justify-content-md-start justify-content-center flex-wrap mx-10 my-5 mx-3">
+
+      <div class="card m-3" style="width: 18rem;">
         <img src="https:/picsum.photos/200" class="card-img-top" alt="Milk">
         <div class="card-body">
           <h5 class="card-title">Piens</h5>
           <p class="card-text">apraksts utt.</p>
-          <p>€1.00</p>
         </div>
+        <div class="card-footer">€1.00</div>
       </div>
 
-      <div class="card col" style="width: 18rem;">
+      <div class="card m-3" style="width: 18rem;">
         <img src="https:/picsum.photos/200" class="card-img-top" alt="Milk">
         <div class="card-body">
           <h5 class="card-title">Piens</h5>
           <p class="card-text">apraksts utt.</p>
-          <p>€1.00</p>
         </div>
+        <div class="card-footer">€1.00</div>
       </div>
 
-      <div class="card col" style="width: 18rem;">
+      <div class="card m-3" style="width: 18rem;">
+        <img src="https:/picsum.photos/200" class="card-img-top" alt="Milk">
+        <div class="card-body">
+          <h5 class="card-title">Piens</h5>
+          <p class="card-text">šis ir tāds mazliet un varbūt mazliet vēl garāks ar vēl dažiem parastiem un ierastiem vārdiem, kur ir apraksts utt.</p>
+        </div>
+          <div class="card-footer">€1.00</div>
+      </div>
+
+      <div class="card m-3" style="width: 18rem;">
         <img src="https:/picsum.photos/200" class="card-img-top" alt="Milk">
         <div class="card-body">
           <h5 class="card-title">Piens</h5>
           <p class="card-text">apraksts utt.</p>
-          <p>€1.00</p>
         </div>
+        <div class="card-footer">€1.00</div>
+      </div>
+
+      <div class="card m-3" style="width: 18rem;">
+        <img src="https:/picsum.photos/200" class="card-img-top" alt="Milk">
+        <div class="card-body">
+          <h5 class="card-title">Piens</h5>
+          <p class="card-text">apraksts utt.</p>
+        </div>
+        <div class="card-footer">€1.00</div>
       </div>
     </div>
-    
-
     
     <!-- End of Product Section -->
 
     <!-- Carousel -->
 
-    <h1 id="galerija">Galerija</h1>
-    
-      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <h1 id="galerija" class="display-5">Galerija</h1>
+    <div class="mx-auto p-2 my-3 container-md">
+      <div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="Photo 1">
+            <img src="https:/picsum.photos/800/400" class="d-block w-100" alt="Photo 1">
           </div>
           <div class="carousel-item">
-            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="Photo 2">
+            <img src="https:/picsum.photos/800/400" class="d-block w-100" alt="Photo 2">
           </div>
           <div class="carousel-item">
-            <img src="https:/picsum.photos/400/200" class="d-block w-100" alt="Photo 3">
+            <img src="https:/picsum.photos/800/400" class="d-block w-100" alt="Photo 3">
           </div>
         </div>
       </div>
-    
+    </div>
 
     <!-- End of Carousel -->
 
     <!-- Contact form -->
 
     
-    <!--Forma ar laukiem: Vārds, E-pasts, Ziņojums un Poga "Sūtīt". -->
-    <h1 class="display-2">Sazinies ar mums</h1>
-    <div class="mb-3">
-      <label for="vardsInput" class="form-label">Vārds</label>
-      <input type="email" class="form-control" id="vardsInput" placeholder="Jānis">
+    <div class="container p-4">
+      <form>
+        <legend>Sazinies ar mums</legend>
+        <div class="mb-3">
+          <label for="vardsInput" class="form-label">Vārds</label>
+          <input type="email" class="form-control" id="vardsInput" placeholder="Jānis">
+        </div>
+        <div class="mb-3">
+          <label for="epastsInput" class="form-label">E-pasts</label>
+          <input type="email" class="form-control" id="epastsInput" placeholder="vards@piemers.com">
+        </div>
+        <div class="mb-3">
+          <label for="zinojums" class="form-label">Ziņojums</label>
+          <textarea class="form-control" id="zinojums" rows="2"></textarea>
+        </div>
+        <div class="d-flex justify-content-center">
+          <button type="submit" class="btn btn-primary">Sūtīt</button>
+        </div>
+      </form>
     </div>
-    <div class="mb-3">
-      <label for="epastsInput" class="form-label">E-pasts</label>
-      <input type="email" class="form-control" id="epastsInput" placeholder="vards@piemers.com">
-    </div>
-    <div class="mb-3">
-      <label for="zinojums" class="form-label">Ziņojums</label>
-      <textarea class="form-control" id="zinojums" rows="2"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Sūtīt</button>
+
+    <!-- End of contact form -->
+
+    <footer class="bg-light py-5"></footer>
 
   </body>
 </html>
