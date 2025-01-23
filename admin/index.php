ef88df2897df7f8cf4d6f24a8370d995c5d4ec7b
Bremzen <aleksisdz6@gmail.com>
2025-01-23 23:05:06 +0200

    //created working admin login page

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Log In</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css?v=2">
+  </head>
+  <body class="d-flex align-items-center justify-content-center">
+    <div class="container bg-body-secondary p-4 rounded">
+        <form action="login.php" method="post">
+            <div class="mt-2 mb-5 text-center">
+                <h1>Log in</h1>
+            </div>
+            <div class="mb-3">
+                <label for="usernameInput" class="form-label">Username</label>
+                <input type="text" class="form-control" id="usernameInput" name="uname" placeholder="Enter username">
+            </div>
+            <div class="mb-3">
+                <label for="passwordInput" class="form-label">Password</label>
+                <input type="password" class="form-control" id="passwordInput" name="password" placeholder="••••••••">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block float-end">Log in</button>
+        </form>
+    </div>
+  </body>
+</html>
---
+++ admin/login.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
+	if (empty($uname)) {
+		header("Location: index.php?error=Username is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}else{
+		$stmt = $conn->prepare("SELECT * FROM users WHERE username=? AND password=?");
+        $stmt->bind_param("ss", $uname, $pass);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            if ($row['username'] === $uname && $row['password'] === $pass) {
+            	$_SESSION['username'] = $row['username'];
+            	$_SESSION['user_id'] = $row['user_id'];
+            	header("Location: home.php");
+            	exit();
+            } else {
+            	header("Location: index.php?error=Invalid username or password");
+            	exit();
+            }
+
+        } else {
+            header("Location: index.php?error=Invalid username or password");
+            exit();
+        }
+        $stmt->close();
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
+}
